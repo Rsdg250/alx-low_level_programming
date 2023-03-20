6ad9153e790f5d2d7a2cff1b318dfcc257d6022a
Solomon <rsdg250@gmail.com>
2023-03-20 16:17:25 -0700

    primt numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program by RSDG that prints all sinle digits numbers of base ten
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+return (0);
+}
