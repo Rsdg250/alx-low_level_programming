3c99d8fc4ee27218a4a2e0d243f80ac91bc08415
Solomon <rsdg250@gmail.com>
2023-03-20 16:59:36 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program by RSDG that prints all posible combinations of single digits numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= 9; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+return (0)
+}
