2c580e0f4186f56c70908b23c251da7fc61fa205
Solomon <rsdg250@gmail.com>
2023-03-20 06:29:13 -0700

    small and capital letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - A program by RSDG that prints all the alphabet in small then in capital letters
+ * main - A program by RSDG that prints small and capital letters
  * Return: Always (success)
  */
 
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 	for (low = 'A'; low <= 'Z'; low++)
