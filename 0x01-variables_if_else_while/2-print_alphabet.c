6a684de437d9032b5305f988ea79438675a6e6d8
Solomon <rsdg250@gmail.com>
2023-03-20 02:37:08 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A progam that prints all the alphabets in small letters
+ * Retun: Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+return (0);
+}
