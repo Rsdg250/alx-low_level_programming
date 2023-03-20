29cec22151893e88eb6b8e02f7c34ce33afe61ad
Solomon <rsdg250@gmail.com>
2023-03-20 15:59:10 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.io>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program by RSDG that prints all sigle digit numbers of base ten
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i =0; i <10; i++)
+	printf("%d", i)
+	printf("\n");
+return (0);
+}
