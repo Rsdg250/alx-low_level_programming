ec6fcd89bbdeea4bd9542ad08be028f308d9c716
Solomon <rsdg250@gmail.com>
2023-03-20 17:22:00 -0700

    combinations

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - A program by RSDG that printa all aphabet in reverse
+ * main - A program by RSDG that prints all aphabet in reverse
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char low;
 
-	for(low = 'z'; low <= 'a'; low++)
+	for (low = 'z'; low >= 'a'; low++)
 	putchar(low);
 	putchar('\n');
 return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - A program by RSDG that prints all posible combinations of single digits numbers
+ * main - A program by RSDG that prints all combination of single digit
  * Return: Always 0 (Success)
  */
 int main(void)
