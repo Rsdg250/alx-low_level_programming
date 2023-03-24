c46bfb6ba440dd6f2288d2585b61282401371e98
Solomon <rsdg250@gmail.com>
2023-03-23 17:41:36 -0700

    more nested loop and function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
 *_isupper - checks whether a character
 *passed to it is lowercase or uppercase
 *character.
-*
 *@c : character to be checked
-*
 *Return: 1 (uppercase), 0 (lowercase)
 */
+
 int _isupper(int c)
 {
 
-	for (c >=65 && c <= 90)
+	for (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
