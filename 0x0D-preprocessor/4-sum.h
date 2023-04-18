43fcc4cedabf04203e1a4963abaf160ae631f840
Solomon <rsdg250@gmail.com>
2023-04-17 17:05:02 -0700

    A function-like macro SUM(x,y) that computes the sume of x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM(x,y) - A function-like macro that cumpute the sum of x and y
+ * @x: first input
+ * @y: second input
+ */
+
+#define SUM(x,y) ((x) + (y))
+
+#endif
