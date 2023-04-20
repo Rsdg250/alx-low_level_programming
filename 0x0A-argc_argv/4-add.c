af2291f6a7063e2ca1bd50538711d2f4a91e61d8
Solomon <rsdg250@gmail.com>
2023-04-20 15:27:55 -0700

    a program sums positive numbers of the argument to the program but returns ERROR when one of the argument is not a number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A function that prints the sum of positive numbers,
+ * Followed by a new line
+ * @argc: The argument count
+ * @argv: An array of pointers to the arguments
+ * Return: If one of the numbers contains symbols that are non-digits - 1
+ * Otherwise - Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
