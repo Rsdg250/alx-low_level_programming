07e8435acf7ec3b541e1f5562337220360ec06b5
Solomon <rsdg250@gmail.com>
2023-04-21 17:37:50 -0700

    A function that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include FUNCTION_POINTERS_H
+
+/**
+ * int_index - A function that searches for an integer
+ * @array: The array the function searches for integers
+ * @size: The size of the array
+ * @cmp: The pointer to function used to find the integer
+ * Return: return -1 if size <= 0
+ * or if array is NULL
+ * or cmp is NULL
+ * index of integer otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		{
+		if (cmp(array[i]) != 0)
+			return (i);
+		}
+	return (-1);
+}
