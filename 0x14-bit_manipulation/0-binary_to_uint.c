1f8331e667959c2b68debb897f936a038955b5b5
Solomon <rsdg250@gmail.com>
2023-05-05 18:29:46 -0700

    bit manipulation - binart to unit.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int integer;
+	int i = 0;
+	unsigned int integer = 0;
 
-	if (b == NULL)
-	{
-	return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-	if (b[i] == '0')
-	{
-		integer = integer << 1;
-	}
-	else if (b[i] == '1')
-	{
-		integer = integer << 1;
-		integer = integer | 1;
-	}
-	else
-		{
+	if (b == 0)
 		return (0);
-		}
+	while (b[i] != '\0')
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		integer = (2 * integer) + (b[i] - '0');
+		i++;
 	}
 	return (integer);
 }
